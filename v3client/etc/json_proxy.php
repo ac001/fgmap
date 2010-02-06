184b995e296e182730c9599e48643e3fec1d432c
Pedro ac001 <ac001@daffodil.uk.com>
2010-02-06 04:51:38 +0000

    icons class and icons

--- v3client/etc/json_proxy.php
+++ v3client/etc/json_proxy.php
@@ -1,44 +1,68 @@
 <?php
 
-$url = 'http://mpmap02.flightgear.org/fg_server_xml.cgi?mpserver02.flightgear.org:5001';
 
-$is_xml = isset($_REQUEST["format"]) && $_REQUEST["format"] == 'xml' ? true : false;
+$fetch = isset($_REQUEST['fetch']) ? $_REQUEST['fetch'] : null;
 
-$xml_string = file_get_contents($url);
-if(substr(0,2) != '<?'){
-	$xml_string = '<?xml version="1.0"?>'.$xml_string;
-}
 
 header('Content-type: text/plain');
-
-//*******************************************************
-//** Return XML
-//*******************************************************
-if($is_xml){
-	echo $xml_string;
-	die;
-}
-
-//*******************************************************
-//** Return Json
-//*******************************************************
-$fields = array('callsign', 'server_ip', 'model', 'lat', 'lng', 'alt', 'heading', 'pitch', 'roll');
-$pilots = array();
-$xml = new DOMDocument();
-$xml->loadXML($xml_string);
-foreach($xml->getElementsByTagName('marker') as $xml_marker_node){
-	$pilot = array();
-	foreach($fields as $field){
-		$pilot[$field] = $xml_marker_node->getAttribute($field);
-	}
-	$pilots[] = $pilot;
-}
-
 $payload = array();
 $payload['success'] = true; //* Extjs needs this to be true, else client side exception
-$payload['count'] = count($pilots);
-$payload['pilots'] = $pilots;
+//echo "fetch=$fetch";
+
+switch($fetch){
+
+	case 'pilots':
+		$url = 'http://mpmap02.flightgear.org/fg_server_xml.cgi?mpserver02.flightgear.org:5001';
+		$xml_string = file_get_contents($url);
+		if(substr($xml_string, 0, 2) != '<?'){  // workround malform
+			$xml_string = '<?xml version="1.0"?>'.$xml_string;
+		}
+		$fields = array('callsign', 'server_ip', 'model', 'lat', 'lng', 'alt', 'heading', 'pitch', 'roll');
+		$pilots = array();
+		$xml = new DOMDocument();
+		$xml->loadXML($xml_string);
+		foreach($xml->getElementsByTagName('marker') as $xml_marker_node){
+			$pilot = array();
+			foreach($fields as $field){
+				$pilot[$field] = $xml_marker_node->getAttribute($field);
+			}
+			$pilots[] = $pilot;
+		}
+
+		$payload['count'] = count($pilots);
+		$payload['pilots'] = $pilots;
+		break;
+
+	case 'servers':
+		// # id::longname::host::port::ip
+		$servers = array();
+		$block = null;
+		$str = file_get_contents('../../fgmap.servers');
+		$arr = explode("\n", $str);
+		foreach($arr as $line){
+			$l = trim($line);
+			if($l == '' || substr($l, 0, 1) == '#'){
+				//skip
+			}elseif(substr($l, 0, 1) == '-'){
+				$block = is_null($block) ? 'Live' : 'Devel';	
+			}else{
+				$sarr = explode('::', $l);
+				$location = substr($sarr[1], strpos($sarr[1], '(') + 1, -1);
+				$servers[] = array('type' => $block, 'id' => $sarr[0], 'longname' => $sarr[1],
+									'host' => $sarr[2], 'port' => $sarr[3], 'ip' => $sarr[4],
+									'location' => $location
+								);
+			}
+		}
+		$payload['servers'] = $servers;
+
+		break;
+
+	default: 
+		$payload['error'] = 'Nothing to do';
+		break;
+
+}
 
 echo json_encode($payload);
-
 ?>
--- v3client/js/FGMap_main.js
+++ v3client/js/FGMap_main.js
@@ -7,15 +7,15 @@ Ext.onReady(function(){
 
 var pilotsStore = new Ext.data.JsonStore({
 	url: "etc/json_proxy.php",
+	baseParams: {fetch: 'pilots'},
 	method: 'GET',
-	baseParams: {},
 	//id: "callsign",
 	root: "pilots",
 	fields: [ 	{name: "callsign", type: 'string'},
 				{name: "server_ip", type: 'string'},
 				{name: "model", type: 'string'},
-				{name: "lat", type: 'string'},
-				{name: "lng", type: 'string'},
+				{name: "lat", type: 'float'},
+				{name: "lng", type: 'float'},
 				{name: "alt", type: 'int'},
 				{name: "heading", type: 'string'},
 				{name: "pitch", type: 'string'},
@@ -43,57 +43,55 @@ var viewport = new Ext.Viewport({
 	layout: 'border',
 	plain: true,
 	items: [
-			//** Left/West area
-			{title: 'Flight Gear Map', region: 'west',
-				split: true,
-				width: 300,
-				minSize: 175,
-				maxSize: 400,
-				collapsible: true,
-				margins: '0 0 0 5',
-				layout: {
-					type: 'fit'
-				},
-				items: new Ext.TabPanel({
-					activeTab: 0,
-					items: [
-						//**** Pilots Lookup Grid
-						new Ext.grid.GridPanel({
-							title: 'Pilots',
-							autoScroll: true,
-							autoWidth: true,
-							tbar:[  //this.actionAdd, this.actionEdit, this.actionDelete, 
-									'-',// this.actionLabSelectToolbarButton,
-									'->',
-									//Geo2.widgets.goto_www('Online', 'View rates on website', '/rates.php'),
-									{text: 'Refresh', iconCls: 'icoRefresh', handler: function(){
-										pilotsStore.reload();
-										}
-									}    
-							],
-							viewConfig: {emptyText: 'No pilots online', forceFit: true}, 
-							//sm: this.selModel,
-							store: pilotsStore,
-							loadMask: true,
-							columns: [  //this.selModel,	
-										{header: 'CallSign',  dataIndex:'callsign', sortable: true},
-										{header: 'Aircraft',  dataIndex:'model', sortable: true},
-										{header: 'Alt', dataIndex:'alt', sortable: true, align: 'right',
-											renderer: function(v, meta, rec, rowIdx, colIdx, store){
-												return "#" + v;
-											}
-										}
-							],
-							listeners: {},
-							bbar: []
-						}),
-						{
-						title: 'Navigation',
-						html: '<p>Some settings in here.</p>',
-						border: false,
-						iconCls: 'settings'
-						}
-					]
+		//** Left/West area
+		{title: 'Flight Gear Map', region: 'west',
+			split: true,
+			width: 300,
+			minSize: 175,
+			maxSize: 400,
+			collapsible: true,
+			margins: '0 0 0 5',
+			layout: {
+				type: 'fit'
+			},
+			items: new Ext.TabPanel({
+				activeTab: 0,
+				items: [
+					//**** Pilots Lookup Grid
+					new Ext.grid.GridPanel({
+						title: 'Pilots',
+						iconCls: 'iconPilots',
+						autoScroll: true,
+						autoWidth: true,
+						tbar:[  //this.actionAdd, this.actionEdit, this.actionDelete, 
+								'-',// this.actionLabSelectToolbarButton,
+								'->',
+								//Geo2.widgets.goto_www('Online', 'View rates on website', '/rates.php'),
+								{text: 'Refresh', iconCls: 'iconRefresh', handler: function(){
+									pilotsStore.reload();
+									}
+								}    
+						],
+						viewConfig: {emptyText: 'No pilots online', forceFit: true}, 
+						//sm: this.selModel,
+						store: pilotsStore,
+						loadMask: true,
+						columns: [  //this.selModel,	
+									{header: 'CallSign',  dataIndex:'callsign', sortable: true},
+									{header: 'Aircraft',  dataIndex:'model', sortable: true}
+						],
+						listeners: {},
+						bbar: []
+					}),
+					//**** Navigation Widget
+					{
+					title: 'Navigation',
+					iconCls: 'iconNavigation',
+					html: '<p>Some settings in here.</p>',
+					border: false,
+					iconCls: 'settings'
+					}
+				]
 			   }) // end tabpanel
             },
             // in this instance the TabPanel is not wrapped by another panel
@@ -102,27 +100,89 @@ var viewport = new Ext.Viewport({
             new Ext.TabPanel({
                 region: 'center', // a center region is ALWAYS required for border layout
                 deferredRender: false,
-                activeTab: 0,     // first tab initially active
+                activeTab: 2, 
                 items: [{
                     contentEl: 'map_canvas',
-                    title: 'Map',
-                    closable: true,
+                    title: 'Map&nbsp;&nbsp;',
+					iconCls: 'iconMap',
                     autoScroll: true
-                }, {
+                }, 
+					//***************************************************
+					//**** Pilots Main Grid
+					new Ext.grid.GridPanel({
+						title: 'Pilots Data',
+						iconCls: 'iconPilots',
+						autoScroll: true,
+						autoWidth: true,
+						tbar:[  //this.actionAdd, this.actionEdit, this.actionDelete, 
+								'-',// this.actionLabSelectToolbarButton,
+								'->',
+								//Geo2.widgets.goto_www('Online', 'View rates on website', '/rates.php'),
+								{text: 'Refresh', iconCls: 'iconRefresh', handler: function(){
+									pilotsStore.reload();
+									}
+								}    
+						],
+						viewConfig: {emptyText: 'No pilots online', forceFit: true}, 
+						//sm: this.selModel,
+						store: pilotsStore,
+						loadMask: true,
+						columns: [  //this.selModel,	
+							{header: 'CallSign',  dataIndex:'callsign', sortable: true},
+							{header: 'Aircraft',  dataIndex:'model', sortable: true},
+							{header: 'Lat', dataIndex:'lat', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Lng', dataIndex:'lng', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Alt', dataIndex:'alt', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Heading', dataIndex:'heading', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Pitch', dataIndex:'pitch', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Roll', dataIndex:'roll', sortable: true, align: 'right',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+							{header: 'Server', dataIndex:'server_ip', sortable: true, align: 'left',
+								renderer: function(v, meta, rec, rowIdx, colIdx, store){
+									return v;
+								}
+							},
+
+						],
+						listeners: {},
+						bbar: []
+					}),
+				//** Server Status
+				{
                     contentEl: 'center2',
                     title: 'Server Status',
+					iconCls: 'iconServerStatus',
                     autoScroll: true
-                }]
+                }
+				]
             })]
         });
-        // get a reference to the HTML element with id "hideit" and add a click listener to it 
-        //Ext.get("hideit").on('click', function(){
-            // get a reference to the Panel that was created with id = 'west-panel' 
-           // var w = Ext.getCmp('west-panel');
-            // expand or collapse that Panel based on its collapsed property state
-           // w.collapsed ? w.expand() : w.collapse();
-        //});
 
-		pilotsStore.load();
-		map_initialize();
-    });
+pilotsStore.load();
+map_initialize();
+
+}); /* Ext.onready() */
+
